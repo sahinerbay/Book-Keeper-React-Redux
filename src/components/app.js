5c52ed47ae4a45858e080b4ab8d304880c254ff5
sahinerbay <sahin@sahinerbay.com>
2017-11-28 14:26:49 +0200

    Update readme instructions

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ const App = () => (
 	<div className="container-fluid">
 		<div className="row">
 			<div className="col-xs-12 col-md-9 col-lg-6 col-centered">
-				<h1>Book Keeper</h1>
+				<h1>Book-Keeper</h1>
 				<AddBook />
 				<AvailableBookList />
 			</div>
